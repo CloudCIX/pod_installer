6be18bcabf6c1bac7c70f0563c459d8e45831152
Maria Walsh <mw@cloudcix.com>
2025-02-07 11:55:11 +0000

    Update version date

--- installer.py
+++ installer.py
@@ -142,7 +142,7 @@ def banner(clear, message):
         stdscr.clear()
     if message == 'header':
         line(1, cols)
-        msg = f'{"CloudCIX Pod Install Utility         Version Date: 7th June 2024":^{cols}}'
+        msg = f'{"CloudCIX Pod Install Utility         Version Date: 7th Feb 2025":^{cols}}'
         stdscr.addstr(2, 1, msg, curses.A_BOLD | curses.A_REVERSE)
         line(3, cols)
     elif message == 'footer':
